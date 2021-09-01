1f60f20880b36d928c461f7bf08faaba5e5de99e
shimon <42068102+shimon94@users.noreply.github.com>
2021-09-01 23:17:19 +0300

    update
    עדכון דף הבית ומוצרים

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products
 
-  root 'home#index'
+  root 'products#index'
   get 'home/about'
   get 'home/dashboard'
-
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
