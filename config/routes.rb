69c18368ad1cf13e02789f37e38a8d3e9cb1849d
shimon <42068102+shimon94@users.noreply.github.com>
2021-09-04 22:28:22 +0300

    s

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products
 
-  root 'products#index'
+  root 'home#index'
   get 'home/about'
   get 'home/dashboard'
   
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
